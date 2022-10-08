cf9db3b6bf61cfa8b81bb9ac7d059588e8b831bb
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-10-08 15:20:48 -0500

    react -typed added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Aos from 'aos';
+import 'aos/dist/aos.css'; 
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
+  Aos.init()
   return (
     <div>
         <Header></Header>
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,21 +1,29 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Banner = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse gap-x-32">
-        <img
-          src="https://i.ibb.co/PZ2XcWD/transparent-bg-designify.png"
-          className="max-w-sm rounded-full shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Hi, I am Nilima Mim</h1>
+          <img
+          data-aos="fade-left"
+            src="https://i.ibb.co/PZ2XcWD/transparent-bg-designify.png"
+            className="md:max-w-sm rounded-full shadow-2xl"
+          />
+        <div data-aos="fade-right" className="text-center">
+          <h1 className="text-5xl font-bold">
+            <Typed strings={["I am Nilima Mim","I am Google advertising specialist"]} typeSpeed={50} backSpeed={50} loop />
+          </h1>
           <p className="py-6">
-          Hello, my name is Nilima Mim. I work as a professional Freelancer. Google advertising specialist I'm a Certified Google Ads, GA4, and Google Tag Manager expert.
+            Hello, my name is I work as a professional Freelancer. Google
+            advertising specialist I'm a Certified Google Ads, GA4, and Google
+            Tag Manager expert.
           </p>
-          <div className="flex gap-x-5">
+          <div className="flex gap-x-5 justify-center md:justify-start">
             <button className="btn">UpWork</button>
-            <button className="btn bg-green-700 active:bg-green-900">Fiverr</button>
+            <button className="btn bg-green-700 active:bg-green-900 hover:bg-green-800">
+              Fiverr
+            </button>
           </div>
         </div>
       </div>
