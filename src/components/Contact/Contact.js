e7e3042e6d36c9c5eb3fb3da0ac8cc1b8c2d8087
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-10-11 07:12:20 -0500

    contect info added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { EnvelopeIcon,PhoneIcon,MapPinIcon } from '@heroicons/react/24/solid'
 
 const Contact = () => {
   const {
@@ -13,16 +14,25 @@ const Contact = () => {
   };
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+      <div className="hero-content flex-col lg:flex-row lg:gap-x-10 w-full">
+        <div className="text-center lg:text-left w-full">
+          <h1 className="text-4xl font-bold before:min-w-full md:block inline-block before:h-0.5 before:bg-gray-400 relative before:absolute before:bottom-[-5px]">Contact Info</h1>
+          <div className="mt-3">
+            <div className="flex gap-x-2 items-center my-2">
+                <EnvelopeIcon className="h-6 w-6 text-success"></EnvelopeIcon>
+                <p className="text-xl">nilimaakterm730@gmail.com</p>
+            </div>
+            <div className="flex gap-x-2 items-center my-2">
+                <PhoneIcon className="h-6 w-6 text-success"></PhoneIcon>
+                <a href="tel:+880 1617-921169" className="text-xl">+880 1617-921169</a>
+            </div>
+            <div className="flex gap-x-2 items-center my-2">
+                <MapPinIcon className="h-6 w-6 text-success"></MapPinIcon>
+                <p className="text-xl">Uttara, Dhaka</p>
+            </div>
+          </div>
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div data-aos="fade-left" className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
           <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
@@ -66,15 +76,15 @@ const Contact = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Description</span>
+                <span className="label-text">Message</span>
               </label>
               <textarea
-              {...register('description',{required: "Description is Require"})}
+              {...register('message',{required: "Message is Require"})}
                 type="text"
-                placeholder="Description"
+                placeholder="Write message"
                 className="input input-bordered min-h-[150px]"
               />
-              {errors.description && <p className="text-red-500 pt-1">{errors.description.message}</p>}
+              {errors.message && <p className="text-red-500 pt-1">{errors.message.message}</p>}
             </div>
             <div className="form-control mt-6">
               <button type='submit' className="btn btn-primary">Send Mail</button>
